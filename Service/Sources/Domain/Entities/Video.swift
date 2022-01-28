ba6d43026fd354f17b9a5a31c615cb3f90189100
baegteun <baegteun@gmail.com>
2022-01-28 14:38:47 +0900

    :sparkles: :: Service - Video

---
+++ Service/Sources/Domain/Entities/Video.swift
@@ -0,0 +1,27 @@
+//
+//  Video.swift
+//  Service
+//
+//  Created by 최형우 on 2022/01/28.
+//
+
+public struct Video: Identifiable, Codable {
+    public let id: Int
+    public let image: String
+    public let duration: Int
+    public let user: User
+    public let videoFiles: [VideoFile]
+    
+    public struct User: Identifiable, Codable {
+        public let id: Int
+        public let name: String
+        public let url: String
+    }
+    
+    public struct VideoFile: Identifiable, Codable {
+        public let id: Int
+        public let quality: String
+        public let fileType: String
+        public let link: String
+    }
+}
