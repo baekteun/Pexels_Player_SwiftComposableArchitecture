6f5833d5e885903d2a42c0a503ffc8df33b2a06b
baegteun <baegteun@gmail.com>
2022-01-28 14:37:39 +0900

    :sparkles: :: Service - Video

--- Service/Sources/Domain/Entities/Query.swift
+++ Service/Sources/Domain/Entities/Query.swift
@@ -8,3 +8,24 @@
 public enum Query: String, CaseIterable{
     case nature, animals, people, ocean, food
 }
+
+public struct Video: Identifiable, Codable {
+    public let id: Int
+    public let image: String
+    public let duration: Int
+    public let user: User
+    public let videoFiles: [VideoFile]
+    
+    public struct User: Identifiable, Codable {
+        public let id: Int
+        public let name: String
+        public let url: String
+    }
+    
+    public struct VideoFile: Identifiable, Codable {
+        public let id: Int
+        public let quality: String
+        public let fileType: String
+        public let link: String
+    }
+}
