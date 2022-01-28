a0657f56a082581f71aa0a36b224db573ec79839
baegteun <baegteun@gmail.com>
2022-01-28 15:38:22 +0900

    :sparkles: :: BaseRemote

--- Service/Sources/Data/Remote/DataSource/BaseRemote.swift
+++ Service/Sources/Data/Remote/DataSource/BaseRemote.swift
@@ -5,6 +5,27 @@
 //  Created by 최형우 on 2022/01/28.
 //
 
+import Alamofire
+import Moya
+import CombineMoya
+import Combine
 import Foundation
 
+class BaseRemote<API: VBAAPI> {
+    private let provider = MoyaProvider<API>(plugins: [PexelsPlugin()])
+    
+    func request(_ api: API) -> AnyPublisher<Response, Error>{
+        if(!NetworkReachabilityManager(host: "https://api.pexels.com")!.isReachable) {
+            return Future<Moya.Response, Error> { result in
+                result(.failure(VBAError.error(message: "서버에 접속할 수 없습니다.")))
+            }
+            .eraseToAnyPublisher()
+        }
+        
+        return provider.requestPublisher(api, callbackQueue: .main)
+            .mapError{ VBAError.error(errorBody: ["status" : $0.response?.statusCode ?? 0]) }
+            .timeout(120, scheduler: DispatchQueue.main, customError: { VBAError.error(message: "요청시간이 만료되었습니다")})
+            .eraseToAnyPublisher()
+    }
+}
 
---
+++ Service/Sources/Domain/Error/VBAError.swift
@@ -0,0 +1,12 @@
+//
+//  VBAError.swift
+//  Service
+//
+//  Created by 최형우 on 2022/01/28.
+//
+
+import Foundation
+
+enum VBAError: Error {
+    case error(message: String = "에러가 발생했습니다.", errorBody: [String:Any] = [:])
+}
